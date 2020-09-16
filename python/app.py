3400bee2b6e663275d6046f29209f708abd88874
Ihanukov <54180291+Ihanukov@users.noreply.github.com>
2020-09-16 11:32:41 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,20 +3,23 @@ import sqlite3
 import json
 import re # regular expressions
 from pprint import pprint
-
+import os.path
 
 app = Flask(__name__)
-DBPATH = "../database.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "database.db") 
 
 @app.route("/messages", methods=["GET"])
-def messages_route():
+def messages():
     """
     Return all the messages
     """
 
-    with sqlite3.connect(DBPATH) as conn:
+    
+   
+    with sqlite3.connect(db_path) as conn:
         messages_res = conn.execute("select body from messages")
-        messages = [m[0] for m in messages_res]
+        messages = [replacemet(m[0]) for m in messages_res]
         return jsonify(list(messages)), 200
 
 
@@ -39,6 +42,25 @@ def search_route():
         print(query, "--> ")
         pprint(answers)
         return jsonify(answers), 200
+    
+ 
+def replacemet(k):
+    data_list = []
+    newstreing = str(k)
+
+    data_list = re.findall(r'{.*?}', newstreing)
+    for match in data_list: # iterate through all matches and create pairs of <id, string>)
+        idword = match.replace("{", "") # we are not interested in the curly brackets
+        idword = idword.replace("}", "")
+        idword = idword.split("|")
+        with sqlite3.connect(db_path) as conn:
+            rep_value = conn.execute("select value from state WHERE id=?", (idword[0],))
+            for row in rep_value:
+             if row[0] != "":
+                newstreing =  newstreing.replace(match, row[0])
+            else:
+                newstreing =  newstreing.replace(match, idword[1])
+    return newstreing
 
 
 if __name__ == "__main__":
